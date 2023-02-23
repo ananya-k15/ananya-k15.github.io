68c99f0d9c0215ab9429f0a1076121befceeedf8
Ananya Kumar <68656129+ananya-k15@users.noreply.github.com>
2023-02-23 00:12:31 -0500

    7 experience page (#8)
    * commit before starting projects
    
    * Add experience section
    
    * Delete unnecessary assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,11 +29,44 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
+/*==================== EDUCATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
 
-/*==================== QUALIFICATION TABS ====================*/
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('experience__active')
+        })
+        target.classList.add('experience__active')
 
+        tabs.forEach(tab =>{
+            tab.classList.remove('experience__active')
+        })
+        tab.classList.add('experience__active')
+    })
+})
+
+/*==================== ACCORDION AWARDS ====================*/
+const awardsContent = document.getElementsByClassName('awards__content'),
+      awardsHeader = document.querySelectorAll('.awards__header')
+
+function toggleAwards(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < awardsContent.length; i++){
+        awardsContent[i].className = 'awards__content awards__close'
+    }
+    if(itemClass === 'awards__content awards__close'){
+        this.parentNode.className = 'awards__content awards__open'
+    }
+}
+
+awardsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleAwards)
+})
 
 /*==================== SERVICES MODAL ====================*/
 
@@ -45,12 +78,67 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-brightness'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-brightness'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
