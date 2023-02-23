8f9340c2682887744bcf5a68f8b772e76c9ee418
Ananya Kumar <ananyakumar5492@gmail.com>
2023-02-22 22:30:52 -0500

    commit before starting projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,11 +29,44 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
+/*==================== EDUCATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
 
-/*==================== QUALIFICATION TABS ====================*/
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('experience__active')
+        })
+        target.classList.add('experience__active')
 
+        tabs.forEach(tab =>{
+            tab.classList.remove('experience__active')
+        })
+        tab.classList.add('experience__active')
+    })
+})
+
+/*==================== ACCORDION AWARDS ====================*/
+const awardsContent = document.getElementsByClassName('awards__content'),
+      awardsHeader = document.querySelectorAll('.awards__header')
+
+function toggleAwards(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < awardsContent.length; i++){
+        awardsContent[i].className = 'awards__content awards__close'
+    }
+    if(itemClass === 'awards__content awards__close'){
+        this.parentNode.className = 'awards__content awards__open'
+    }
+}
+
+awardsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleAwards)
+})
 
 /*==================== SERVICES MODAL ====================*/
 
@@ -54,3 +87,31 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-brightness'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-brightness'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
